6892fdb521212fcd36eeabf8862bc1db16437bee
P4t4m8n <eranopenuni@gmail.com>
2024-03-21 12:47:37 +0200

    Update nav to link

--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function MovieItem({ movie, onMovieSelect }) {
+function MovieItem({ movie }) {
   return (
     <li className='movie-item' key={movie.episodeId}>
       {/* //Call back props was not sent to parent */}
       <img src={movie.imgUrl} alt=''></img>
-      <button onClick={() => onMovieSelect(movie)}>
+      <button><Link to={`/${movie.episodeId}`}>
         {movie.title}
+      </Link>
       </button>
     </li>
   );
 }
 
-export default MovieItem;
+export default MovieItem; 
