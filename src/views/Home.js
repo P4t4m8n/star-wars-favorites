f5c7319e50f03c8f0f9aeb4451f928c5239988d9
P4t4m8n <eranopenuni@gmail.com>
2024-03-22 10:22:47 +0200

    Move data loading to home

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,14 +1,28 @@
-// App.js
-
-import React from 'react';
+import React from 'react'
+import { itemService } from '../services/item.service'
+import { useLoading } from '../hooks/useLoading'
+import Loading from '../components/Loading/Loading'
+import { useEffectUpdate } from '../hooks/useEffectUpdate'
 
 
 function Home() {
+  const { isLoading, toggleLoading } = useLoading()
+  useEffectUpdate(loadItems, [])
+
+  async function loadItems() {
+    try {
+      await itemService.makeData()
+    } catch (error) {
+      console.error("Unable to make data:", error)
+    } finally { toggleLoading() }
+  }
+
+  if (isLoading) return (<Loading />)
   return (
     <section className="home">
-        <h1>Welcome to the Star Wars Universe</h1>
+      <h1>Welcome to the Star Wars Universe</h1>
     </section>
-  );
+  )
 }
 
-export default Home;
+export default Home
